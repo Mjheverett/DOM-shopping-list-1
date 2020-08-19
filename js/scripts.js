10c646bee1077e9b067487e3d1709fbfc42a229e
Matthew Everett <matthew@mjheverett.com>
2020-08-19 12:07:59 -0400

    fixing fork/clone error

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const myList = document.getElementById('myList');
+const inputItems = document.querySelectorAll('input');
+const parentList = document.createElement('ul');
+myList.appendChild(parentList);
+
+function addItemsToList(inputItems, parentList) {
+    inputItems.forEach(function(item) {
+        const itemName = item.value;
+        const itemList = document.createElement('li');
+        itemList.innerHTML = itemName;
+        parentList.appendChild(itemList);
+    });
+};
+
+generateList.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    addItemsToList(inputItems, parentList);
+});
